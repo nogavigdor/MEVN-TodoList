f78aa06d59b4a8e943ee32db865c1c09cea65611
Noga Vigdor <noga.vigdor@gmail.com>
2024-03-05 00:00:42 +0100

    connect to mongodb

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,10 @@
-//mongodb+srv://nognog:<password>@cluster0.ui9b3ig.mongodb.net/
+
 
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+
+require('dotenv').config(); // Load environment variables from .env file
 
 //create express app
 const app = express();
@@ -14,3 +16,25 @@ app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Headers", "auth-token, Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
+
+//database connection
+const uri = process.env.DBHOST;
+mongoose.connect(uri, {
+    // useNewUrlParser: true, 
+   //  useUnifiedTopology: true
+    })
+    .then(() => console.log('Connected to MongoDB'))
+    .catch(err => console.log(err));
+
+    app.use(bodyParser.json());
+
+    app.get('/', (req, res) => {
+        res.send('Hello from Home Page');
+    });
+
+    //start server
+    const PORT = process.env.PORT || 3000;
+
+    app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`);
+    });
