91c5ed891fa4602d411ff5f98f22b0ba98cdd89e
Noga Vigdor <noga.vigdor@gmail.com>
2024-03-05 00:49:22 +0100

    set todo POST route

--- backend/routes/Todos.js
+++ backend/routes/Todos.js
@@ -1,13 +1,31 @@
 const express = require('express');
 const router = express.Router();
-const Todos = require('../models/Todos');
+const Todo = require('../models/Todos');
 
 //Get all todos route
 router.get('/', async (req, res) => {
     try {
-        const todos = await Todos.find();
+        const todos = await Todo.find();
         res.json(todos);
     } catch (err) {
         res.json({ message: err });
     }
 });
+
+//Post a todo route
+router.post('/new', async (req, res) => {
+    const todo = new Todo({
+        
+            todo: "wash the dishes",
+            author: "Noga Vigdor"
+
+        }
+    );
+
+    const savedTodo = await newTodo.save();
+    res.json(savedTodo);
+});
+
+
+
+module.exports = router;
