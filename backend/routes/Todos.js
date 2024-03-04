d18178b0176436a52a073f3c30261d38c655730e
Noga Vigdor <noga.vigdor@gmail.com>
2024-03-05 00:59:07 +0100

    setting a route to GET a specific toto

--- backend/routes/Todos.js
+++ backend/routes/Todos.js
@@ -26,6 +26,16 @@ router.post('/new', async (req, res) => {
     res.json(savedTodo);
 });
 
+//setting a GET route for a specific todo
+router.get('/get/:id', async (req, res) => {
+    try {
+        const todo = await Todo.findById({_id: req.params.id});
+        res.json(todo);
+    } catch (err) {
+        res.json({ message: err });
+    }
+});
+
 
 
 module.exports = router;
